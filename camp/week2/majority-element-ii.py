1c06c120dd06387764338917b9fee78648696080
Abenezer Workineh <109088982+AbenezerWork@users.noreply.github.com>
2023-12-06 17:20:52 +0300

    Added solution for Majority Element II

--- camp/week2/majority-element-ii.py
+++ camp/week2/majority-element-ii.py
@@ -2,14 +2,14 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         check = len(nums)//3
         dic = {}
-        ans = []
+        ans = set()
         for i in nums:
             if i in dic:
                 dic[i]+=1
             else:
                 dic[i]=1
-        for i in dic:
-            if dic[i] > check:
-                ans.append(i)
+            if dic[i]>check:
+                ans.add(i)
+        
     
-        return ans
+        return list(ans)
