84802e8ff1225b045613d1bdf0a36bded57fb5cd
Abenezer Workineh <109088982+AbenezerWork@users.noreply.github.com>
2024-02-29 10:57:50 +0300

    Add solution for Predict the Winner

--- a2sv/actualcamp/week1/leetcode/predict-the-winner.py
+++ a2sv/actualcamp/week1/leetcode/predict-the-winner.py
@@ -1,15 +1,16 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        return self.solve(nums, 1, 0, len(nums)-1)>=0
-
-    def solve(self, nums, t, st, en):
-        if st == en:
-            return t*nums[en]
-
-        left = self.solve(nums, t*-1, st+1, en)+t*(nums[st])
-        right = self.solve(nums, t*-1, st, en-1)+ t*(nums[en])
-
-        if t == 1:
-            return max(left, right)
-        else:
-            return min(left, right)
+        @cache
+        def solve(t,st, en):
+            if st == en:
+                return t*nums[en]
+    
+            left = solve( t*-1, st+1, en)+ t*(nums[st])
+            right = solve(t*-1, st, en-1)+ t*(nums[en])
+    
+            if t == 1:
+                return max(left, right)
+            else:
+                return min(left, right)
+        return solve( 1, 0, len(nums)-1)>=0
+    
