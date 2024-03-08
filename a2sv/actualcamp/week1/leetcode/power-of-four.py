fbb558326d41b94c99dbbb418e1b48981d47f3e2
Abenezer Workineh <109088982+AbenezerWork@users.noreply.github.com>
2024-03-08 16:54:38 +0300

    Add solution for Power of Four

--- a2sv/actualcamp/week1/leetcode/power-of-four.py
+++ a2sv/actualcamp/week1/leetcode/power-of-four.py
@@ -1,9 +1,5 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        return self.solve(n)
-    def solve(self,n):
-        if n<1:
-            return False
-        if n==1:
-            return True
-        return self.solve(n/4)
+        if n<=1:
+            return n==1
+        return self.isPowerOfFour(n/4)
